60ad69337e6e00d836c6f20201bd1356d0220672
beaming-subroutine-5164 <ben.garlock@gmail.com>
2020-06-16 14:12:49 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -34,9 +34,12 @@ def initial_round
   # code #initial_round here
   card_1 = deal_card
   card_2 = deal_card
+  sum = card_1 + card_2
+  puts display_card_total(sum)
+  
   return card_1 + card_2
   
-  return display_card_total(card_1 + card_2)
+
   
 end
 
